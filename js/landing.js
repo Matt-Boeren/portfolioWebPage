e1b344d476bd5e9d1b676386f62d859390e5a91c
MattBoeren <r0932402@student.thomasmore.be>
2024-09-18 22:46:55 +0200

    landings page

---
+++ js/landing.js
@@ -0,0 +1,42 @@
+const container = document.getElementById("innerBody");
+
+const urlParams = new URLSearchParams(window.location.search);
+const id = parseInt(urlParams.get('id'));
+console.log(id);
+fetch('files/projects.json')
+  .then(response => response.json())
+  .then(data => {
+    const jsonData = data.projects;
+
+    if (Array.isArray(jsonData)) {
+      let htmlContent = ''; // Declare htmlContent once
+      for (let i = 0; i < jsonData.length; i++) {
+				console.log(i);
+        const project = jsonData[i];
+        const projectID = project.id;
+				console.log(id);
+				console.log(projectID); 
+        if (id === projectID) {
+          const imageUrls = project.images || []; // Ensure imageUrls is defined
+          const title = project.title || 'No title provided';
+          const page = project.page || ''; // Ensure page is defined
+
+          htmlContent += `<h1>${title}</h1>`;
+					document.title = title;
+          for (let j = 0; j < imageUrls.length; j++) {
+            htmlContent += `<img src="${imageUrls[j]}" alt="${title} ${j}">`;
+          }
+          htmlContent += page;
+          break; // Exit loop once the matching project is found
+        }
+      }
+
+      container.innerHTML = htmlContent;
+      console.log(htmlContent);
+    } else {
+      console.error('Projects data is not an array:', jsonData);
+    }
+  })
+  .catch(error => {
+    console.error('Error fetching projects:', error);
+  });
---
+++ js/listTopProjects.js
@@ -0,0 +1,36 @@
+const container = document.getElementById("projects");
+
+fetch('files/projects.json')
+  .then(response => response.json())
+  .then(data => {
+    const jsonData = data.projects;
+
+    console.log('Projects Data:', jsonData);
+
+    if (Array.isArray(jsonData)) {
+      let htmlContent = '';
+      for (let i = 0; i < jsonData.length; i++) {
+        const project = jsonData[i];
+        const imageUrl = project.images[0] || 'default-image.jpg'; 
+				const altText = project.title || 'No title provided';
+				const id = project.id;
+				let link = '#';
+				if(project.id !== undefined){
+					link = 'landingPage.html?id=' + id;
+				}
+
+        htmlContent += `
+					<a href="${link}">
+            <img src="${imageUrl}" alt="${altText}">
+						${project.title}
+					</a>
+        `;
+      }
+      container.innerHTML = htmlContent;
+    } else {
+      console.error('Projects data is not an array:', jsonData);
+    }
+  })
+  .catch(error => {
+    console.error('Error fetching projects:', error);
+  });
