69cc4defe6cd9477d90ca95e84c34a000661ca46
Matt-Boeren <matt.boeren@hotmail.com>
2025-02-11 14:01:46 +0100

    console removed

--- js/listTopProjects.js
+++ js/listTopProjects.js
@@ -5,7 +5,6 @@ fetch('files/projects.json')
   .then(data => {
     const jsonData = data.projects;
 
-    console.log('Projects Data:', jsonData);
 
     if (Array.isArray(jsonData)) {
       let htmlContent = '';
